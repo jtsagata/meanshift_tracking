ad0919f444cb656a6653f64708acda9e536859bc
Giannis Tsagatakis <jtsagata@gmail.com>
2018-10-28 00:35:51 +0200

    Rectangles as {tb, size}

--- Code/xRoi.m
+++ Code/xRoi.m
@@ -1,74 +1,145 @@
 classdef xRoi
-    %UNTITLED12 Summary of this class goes here
+    %xRoi A region of interest
     %   Detailed explanation goes here
     
     properties
-        cx
-        cy
-        width
-        height
+        tl, size
     end
     
+    properties (Dependent)
+        center
+    end
+    
+    properties (Dependent, Hidden)
+        area, br
+    end
+    
+    
     methods
-        function obj = xRoi(cx,cy,width,height)
-            %UNTITLED12 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.cx = round(cx);
-            obj.cy = round(cy);
-            obj.width = round(width);
-            obj.height = round(height);
-            
-        end
-        
-        function new_roi = scale(self,k)
-            new_width = round(self.width);
-            new_height = round(self.height);
-            new_roi = xRoi(self.cx,self.cy,new_width*k,new_height*k);
-        end
-        
-        function r=x(self) 
-            r = round(max(1, self.cx - self.width/2));
-        end
-        
-        function r=y(self)
-            r = round(max(1, self.cy - self.height/2));
-        end
-        
-        function imPatch=getRoiImage(self, image) 
-            % This function extract an image patch from an image
-
-            ImWidth = size(image,2);
-            ImHeight = size(image,1);
-            
-            x2 = round( min(ImWidth,  self.x+self.width));
-            y2 = round( min(ImHeight, self.y+self.height)); 
-            
-            imPatch = image(self.y:y2, self.x:x2); 
-        end
-        
-        function imageOut = annotate(self,image)
-            if  ismatrix(image) && ndims(image) == 2
-                % Convert frame to RGB
-                image=cat(3,image);
+        function roi = xRoi(varargin)
+            if nargin ==2
+                % Create from xRoi[x1,x2],[w,h])
+                x1 = round(varargin{1}(1));
+                y1 = round(varargin{1}(2));
+                w  = round(varargin{2}(1));
+                h  = round(varargin{2}(2));                
+            elseif nargin ==3
+                % Create from xRoi([cx,cy],w,h)
+                cx = varargin{1}(1);
+                cy = varargin{1}(2);
+                w  = varargin{2};
+                h  = varargin{3};
+                if mod(w,2) == 0
+                    x1 = round(cx - w/2)+1;
+                else
+                    x1 = round(cx - w/2);
+                end
+                if mod(w,2) == 0
+                    y1 = round(cy - h/2)+1;
+                else
+                    y1 = round(cy - h/2);                    
+                end
+            elseif nargin ==4
+                % Create from xRoi(x1,y1,x2,y2)
+                x1 = round(varargin{1});
+                y1 = round(varargin{2});
+                x2 = round(varargin{3});
+                y2 = round(varargin{4});
+                w = x2-x1+1;
+                h = y2-y1+1;
             end
-               
-            imageOut = insertMarker(image,[self.cx,self.cy],'x', 'color','red');
-             
-            x1 = self.cx - self.width/2;
-            y1 =  self.y;
-            x2 = round( min(self.width,  x1+self.width+1));
-            y2 = round( min(self.height, y1+self.height+1));
+            roi.tl   = [x1,y1];
+            roi.size = [w,h];
+            assert ( (roi.size(1) >=0), 'Negative width');
+            assert ( (roi.size(2) >=0), 'Negative height');
+        end
+        
+        function center=get.center(self)
+            cx= round(self.tl(1) + self.size(1)/2) -1;
+            cy= round(self.tl(2) + self.size(2)/2) -1;
+            center=[cx,cy];
+        end
+        
+        function br=get.br(self)
+            br=[self.tl(1)+self.size(1)-1,self.tl(2)+self.size(2)-1];
+        end
+        
+        function area = get.area(self)
+            area = self.size(1) * self.size(2);
+        end
+        
+        function new_roi = scale(self,sx,sy)
+            if nargin == 3
+                width = self.size(1) *sx;
+                height = self.size(2) *sy;
+            else
+                width = self.size(1) *sx;
+                height = self.size(2) *sx;
+            end
+            new_roi = xRoi(self.center, round(width), round(height));
+        end
 
-            
-            imageOut = insertShape(imageOut,'rectangle', ...
-                [self.x, self.y, x2, y2], ...
-                'LineWidth', 2, 'Color', 'green');
-        end
         
-        function r=area(self)
-            r = self.width * self.height;
-        end
-        
+%         
+%         function [imPatch,imRoi]=getRoiImage(self, image) 
+%             % This function extract an image patch from an image
+% 
+%             ImWidth = size(image,2);
+%             ImHeight = size(image,1);
+%             
+%             x2 = round( min(ImWidth,  self.x+self.width));
+%             y2 = round( min(ImHeight, self.y+self.height)); 
+%             
+%             imPatch = image(self.y:y2, self.x:x2);
+%             
+%             imRoi = 0;
+%         end
+%         
+%         function imageOut = annotate(self,image)
+%             if  ismatrix(image) && ndims(image) == 2
+%                 % Convert frame to RGB
+%                 image=cat(3,image);
+%             end
+%                
+%             imageOut = insertMarker(image,[self.cx,self.cy],'x', 'color','red');
+%              
+%             x1 = self.cx - self.width/2;
+%             y1 =  self.y;
+%             x2 = round( min(self.width,  x1+self.width+1));
+%             y2 = round( min(self.height, y1+self.height+1));
+% 
+%             
+%             imageOut = insertShape(imageOut,'rectangle', ...
+%                 [self.x, self.y, x2, y2], ...
+%                 'LineWidth', 2, 'Color', 'green');
+%         end
+%         
+%         function roi=intersect(self,image)
+%             % This function claculate the intersection roi
+% 
+%             % Note that it is height,width
+%             imageH = size(I,1); imageW = size(I,2);
+% 
+%             y1 = max(1, floor(self.cy - self.height/2));
+%             x1 = max(1, floor(self.cx - self.width/2));
+%     
+%             y2 = min(imageH, ceil(self.cy + self.height/2));
+%             x2 = min(imageW, ceil(self.cx + self.width/2));
+%             
+%             
+% 
+%     
+% %     r2 = round(min(imageH, y1+h+1));
+% %     c2 = round(min(imageW, x+w+1));
+% %     r = round(max(y1, 1));
+% %     c = round(max(x, 1));
+% %     imPatch = I(r:r2, c:c2, :);  
+%         end
+%         
+%         function r=area(self)
+%             r = self.width * self.height;
+%         end
+%         
          
     end
 end
--- Code/xRoi_test.m
+++ Code/xRoi_test.m
@@ -1,30 +1,79 @@
 % test xRoI operations
 
+%%% Even dimensions
+
+% Create from xRoi([cx,cy],w,h)
+r =xRoi([7,7],9,7);
+assert(isequal(r.center,[7 7]));
+assert(isequal(r.size,  [9 7]));
+assert(isequal(r.tl,    [3 4]));
+assert(isequal(r.br,    [11 10]));
+
+% Create from xRoi[x1,x2],[w,h])
+r =xRoi([3,4],[9,7]);
+assert(isequal(r.center,[7 7]));
+assert(isequal(r.size,  [9 7]));
+assert(isequal(r.tl,    [3 4]));
+assert(isequal(r.br,    [11 10]));
+
+% Create from xRoi(x1,x2,y1,y2)
+r =xRoi(3,4,11,10);
+assert(isequal(r.center,[7 7]));
+assert(isequal(r.size,  [9 7]));
+assert(isequal(r.tl,    [3 4]));
+assert(isequal(r.br,    [11 10]));
+
+%%% Odd dimensions
+
+% Create from xRoi(x1,x2,y1,y2)
+r = xRoi(1,1,4,6);
+assert(isequal(r.center,[2 3]));
+assert(isequal(r.size,  [4 6]));
+assert(isequal(r.tl,    [1 1]));
+assert(isequal(r.br,    [4 6]));
+
+% Create from xRoi[x1,x2],[w,h])
+r = xRoi([1,1],[4,6]);
+assert(isequal(r.center,[2 3]));
+assert(isequal(r.size,  [4 6]));
+assert(isequal(r.tl,    [1 1]));
+assert(isequal(r.br,    [4 6]));
+
+% Create from xRoi([cx,cy],w,h)
+r = xRoi([2,3],4,6);
+assert(isequal(r.size,  [4 6]));
+assert(isequal(r.tl,    [1 1]));
+assert(isequal(r.br,    [4 6]));
+assert(isequal(r.center,[2 3]));
+
+
+return
 clearvars
 close all
 load("cars.mat");
+od = repmat({':'},1,ndims(Video)-1);
 ROI=xRoi(ROI_Center(1),ROI_Center(2),ROI_Width,ROI_Height);
 
-od = repmat({':'},1,ndims(Video)-1);
+
 
 figure
 frame = Video(od{:},1);
+% 
+% tImg = ROI.annotate(frame);
+% subplot(2,3,[1 2 4 5]);imshow(tImg);
+% 
+% patch = ROI.getRoiImage(frame);
+% subplot(2,3,3);imshow(patch,[]);
+% 
+% return
 
-tImg = ROI.annotate(frame);
-subplot(2,3,[1 2 4 5]);imshow(tImg);
-
-patch = ROI.getRoiImage(frame);
-subplot(2,3,3);imshow(patch,[]);
-
-return
-
-for h = 1:ROI_Height
+for h = 1:Video_Height
     roi1 = xRoi(ROI_Center(1), h, ROI_Width,ROI_Height);
     
     tImg = roi1.annotate(frame);
     subplot(2,3,[1 2 4 5]);imshow(tImg,[]);
     
-    patch = roi1.getRoiImage(frame);
+    [patch, patchROI] = roi1.getRoiImage(frame);
     subplot(2,3,3);imshow(patch,[]);
     
     drawnow;
